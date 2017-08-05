1fe4809bc274309820398c49750ede17eadf7fad
Bryan Lopez <petitemorth@gmail.com>
2017-08-05 13:45:15 -0500

    Ajustes número de test n!

--- src/problem4/Problem4.java
+++ src/problem4/Problem4.java
@@ -17,19 +17,26 @@ public class Problem4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Creamos la variable donde almacenamos el número de entrada
-        int n;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el número: ");
-        //Asignamos el número de entrada a la variable
-        n = sc.nextInt();
-        
         //Creamos un objeto de la misma clase para hacer uso de la función para calcular el factorial
         Problem4 p4 = new Problem4();
-        //Guardamos el resultado en tipo de dato Long
-        long fac = p4.factorial(n);
-        //Imprimimos el resultado de ceros
-        System.out.println("La cantidad de los últimos ceros son: " + p4.getZeros(fac));
+        
+        //Creamos la variable donde almacenamos el número de entrada
+        int n_test, n;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el número de tests: ");
+        //Asignamos el número de tests
+        n_test = sc.nextInt();
+        
+        for (int i = 1; i <= n_test; i++) {
+            System.out.println("Ingrese el número: ");
+            //Asignamos el número de entrada a la variable
+            n = sc.nextInt();
+            //Guardamos el resultado en tipo de dato Long
+            long fac = p4.factorial(n);
+            //Imprimimos el resultado de ceros
+            System.out.println("La cantidad de los últimos ceros son: " + p4.getZeros(fac));
+        }
     }
     
     /*
